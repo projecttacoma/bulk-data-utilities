583240a8186c21895d9852864e72858c3a6ee6c1
natjoe4 <njjones@mitre.org>
2021-10-12 15:39:00 -0400

    exported the correct function

--- src/bulkImportWrappers.ts
+++ src/bulkImportWrappers.ts
@@ -2,7 +2,7 @@ import { retrieveBulkDataFromMeasureBundle } from './RequirementsQuery';
 import { populateDB } from './utils/ndjsonParser';
 import { assembleTransactionBundles } from './utils/bundleAssemblyHelpers';
 
-async function executeBulkImport(mb: fhir4.Bundle, location: string) {
+export async function executeBulkImport(mb: fhir4.Bundle, location: string) {
   const bulkDataResponses = await retrieveBulkDataFromMeasureBundle(mb);
   const db = await populateDB(bulkDataResponses, location);
   return await assembleTransactionBundles(db);
