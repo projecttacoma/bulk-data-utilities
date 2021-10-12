df5a05b8351d5c383f6f9831e6f3179308895253
natjoe4 <njjones@mitre.org>
2021-10-12 10:16:33 -0400

    removed run.ts

--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,4 @@
-import { assembleTransactionBundle } from './utils/bundleAssemblyHelpers';
+//import { assembleTransactionBundle } from './utils/bundleAssemblyHelpers';
 
 // assembleTransactionBundle('test/ndjsonResources/simple', './database.db').then(bundle =>
 //   console.log(JSON.stringify(bundle, null, 4))
