8c6ed2facad179a5f27df148554dea8abfb9641b
natjoe4 <njjones@mitre.org>
2021-10-12 15:11:45 -0400

    changed index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * as RequirementsQuery from './RequirementsQuery';
 export * as ndjsonParser from './utils/ndjsonParser';
 export * as bundleAssemblyHelpers from './utils/bundleAssemblyHelpers';
-export * as executeBulkImport from './bulkImport';
+export * as BulkImportWrappers from './bulkImportWrappers';
