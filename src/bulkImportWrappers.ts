b2faa1dda744892e2345deeeb4d110eb9bbb7c29
mgramigna <mgramigna@mitre.org>
2022-01-12 15:30:15 -0500

    Remove walk and add flag for useTypeFilters

--- src/bulkImportWrappers.ts
+++ src/bulkImportWrappers.ts
@@ -1,14 +1,20 @@
 import { retrieveAllBulkData, retrieveBulkDataFromMeasureBundle } from './utils/requirementsQueryBuilder';
 import { BulkDataResponse } from './types/requirementsQueryTypes';
 
-export async function executeBulkImport(exportUrl: string, mb?: fhir4.Bundle): Promise<BulkDataResponse[] | undefined> {
+export async function executeBulkImport(
+  exportUrl: string,
+  mb?: fhir4.Bundle,
+  useTypeFilters?: boolean
+): Promise<BulkDataResponse[] | null> {
   let bulkDataResponses = null;
   if (mb) {
-    ({ output: bulkDataResponses } = await retrieveBulkDataFromMeasureBundle(mb, exportUrl));
+    ({ output: bulkDataResponses } = await retrieveBulkDataFromMeasureBundle(mb, exportUrl, useTypeFilters));
   } else {
     ({ output: bulkDataResponses } = await retrieveAllBulkData(exportUrl));
   }
   if (bulkDataResponses) {
     return bulkDataResponses;
   }
+
+  return null;
 }
