af76edfdcb1cf7ce9b0ca19723ba54678e7c1fb6
mgramigna <mgramigna@mitre.org>
2022-04-01 08:54:34 -0400

    Disable all sqlite code and remove dependency

--- src/utils/ndjsonParser.ts
+++ src/utils/ndjsonParser.ts
@@ -1,6 +1,10 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 
-import * as sqlite3 from 'sqlite3';
+/* NOTE: currently disabling the sqlite3 code until a decision is made on referential integrity in ndjson
+   right now, it kills performance
+*/
+
+/* import * as sqlite3 from 'sqlite3';
 // wrapper around sqlite3 that is promise-based
 import * as sqlite from 'sqlite';
 import { BulkDataResponse } from '../types/requirementsQueryTypes';
@@ -8,14 +12,14 @@ import { retrieveNDJSONFromLocation } from './ndjsonRetriever';
 
 // Used to log number of resources inserted into the db
 let insertedResources = 0;
-
+ */
 /**
  * takes in the desired path to a db and creates the db with appropriate tables
  * in that location
  * @param location the file path to where you want the directory created
  * @returns a Promise which resolves to a sqlite database
  */
-export async function createDatabase(location: string): Promise<sqlite.Database> {
+/* export async function createDatabase(location: string): Promise<sqlite.Database> {
   const DB = await sqlite.open({
     filename: location,
     driver: sqlite3.Database
@@ -41,7 +45,7 @@ export async function createDatabase(location: string): Promise<sqlite.Database>
   );
   return DB;
 }
-
+ */
 /**
  * Converts the passed in json string to a valid fhir_resources entry and adds it to fhir_resources,
  * then finds all local references of that resource and adds them to local_references table
@@ -49,7 +53,7 @@ export async function createDatabase(location: string): Promise<sqlite.Database>
  * @param line a json string representing a fhir_resource
  * @returns
  */
-export async function insertResourceIntoDB(DB: sqlite.Database, line: string): Promise<void> {
+/* export async function insertResourceIntoDB(DB: sqlite.Database, line: string): Promise<void> {
   const json = JSON.parse(line);
   const localRefsArray = getLocalReferences(json);
   await DB.run(
@@ -71,20 +75,18 @@ export async function insertResourceIntoDB(DB: sqlite.Database, line: string): P
   }
   insertedResources += 1;
 }
-
+ */
 /**
  * Traverses the reference graph in the sqlite db and determines if all references are valid
  * @param db a sqlite database we want to check the references of
  */
-export async function checkReferences(db: sqlite.Database): Promise<void> {
+/* export async function checkReferences(db: sqlite.Database): Promise<void> {
   const params = {
     $limit: 1000,
     $offset: 0
   };
 
-  /**
-   * returns an array of size $limit of rows from the db, starting at row: $offset
-   */
+  //returns an array of size $limit of rows from the db, starting at row: $offset
   const getRowSet = async () => await db.all('SELECT * FROM "fhir_resources" LIMIT $limit OFFSET $offset', params);
 
   const checkRow = async (row: { resource_json: string; fhir_type: string; resource_id: string }): Promise<any> => {
@@ -134,6 +136,7 @@ export async function checkReferences(db: sqlite.Database): Promise<void> {
   };
   await checkRowSet();
 }
+*/
 
 /**
  * Gets all references from fhir object. must be recursive since we
@@ -143,7 +146,7 @@ export async function checkReferences(db: sqlite.Database): Promise<void> {
  */
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export function getLocalReferences(entry: any): string[] {
+/* export function getLocalReferences(entry: any): string[] {
   const targetKey = 'reference';
   const references: string[] = [];
   if (typeof entry !== 'object') {
@@ -157,7 +160,7 @@ export function getLocalReferences(entry: any): string[] {
   });
 
   return references;
-}
+} */
 
 /**
  * takes in a directory containing ndjson files and creates and populated a db with fhir_resources
@@ -166,7 +169,7 @@ export function getLocalReferences(entry: any): string[] {
  * @param location a file path signifying where to store the created db
  * @returns a promise which resolves to a sqlite3 db
  */
-export async function populateDB(exportOutput: BulkDataResponse[], location: string): Promise<sqlite.Database> {
+/* export async function populateDB(exportOutput: BulkDataResponse[], location: string): Promise<sqlite.Database> {
   const DB: sqlite.Database = await createDatabase(location);
   // We want each ndjson file to be parse synchronously here to avoid overloading the memory
   // This forces us to use a reduce which waits for the previous iteration to return before executing the next
@@ -187,4 +190,4 @@ export async function populateDB(exportOutput: BulkDataResponse[], location: str
   console.log('\nValidation complete');
   console.log(`${insertedResources} resources inserted in DB`);
   return DB;
-}
+} */
